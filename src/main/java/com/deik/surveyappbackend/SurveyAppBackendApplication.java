985aafc78e86f355823bcb1757773d79a3227e6e
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-10 09:44:38 +0100

    Project created and connected to the database

---
+++ src/main/java/com/deik/surveyappbackend/SurveyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deik.surveyappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyAppBackendApplication.class, args);
+    }
+
+}
