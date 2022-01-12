775fabb5d8efab8a5bda330cdc5e45c5676f13ce
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-12 12:11:33 +0100

    Creating needed jwt methods

--- src/main/java/com/deik/surveyappbackend/SurveyAppBackendApplication.java
+++ src/main/java/com/deik/surveyappbackend/SurveyAppBackendApplication.java
@@ -2,20 +2,10 @@ package com.deik.surveyappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SurveyAppBackendApplication {
-
-    @RequestMapping("/hello")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SurveyAppBackendApplication.class, args);
     }
-
 }
