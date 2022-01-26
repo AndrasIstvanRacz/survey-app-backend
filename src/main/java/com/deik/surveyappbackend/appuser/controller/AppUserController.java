5709f2719770387bf934d6de34a1eeaa1fb7171a
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-01-26 09:40:54 +0100

    CrossOrigin added

--- src/main/java/com/deik/surveyappbackend/appuser/controller/AppUserController.java
+++ src/main/java/com/deik/surveyappbackend/appuser/controller/AppUserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class AppUserController {
