2e57369675cb7e2a5fe983a61722c61d89e283d7
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-03-19 12:41:53 +0100

    SecurityConfig updated

--- src/main/java/com/deik/surveyappbackend/security/SecurityConfig.java
+++ src/main/java/com/deik/surveyappbackend/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/survey/getByIdWithoutAuth",
