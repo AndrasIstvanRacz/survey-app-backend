bc41b82b45123fc57c080810c2b890e4f365f69f
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2023-02-27 10:53:26 +0100

    Cors update

--- src/main/java/com/deik/surveyappbackend/survey/controller/SurveyController.java
+++ src/main/java/com/deik/surveyappbackend/survey/controller/SurveyController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/survey")
 @RequiredArgsConstructor
 public class SurveyController {
